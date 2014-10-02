74cbd9e902cfe1e7cbcddcfaf9f193c7682398ea
andrewliebchen <andrewliebchen@gmail.com>
2014-10-01 23:07:10 -0700

    Take a stab at returning useful things

--- css-status.js
+++ css-status.js
@@ -1,11 +1,15 @@
 Stylesheets = new Meteor.Collection('stylesheets');
 
 if (Meteor.isClient) {
+  Template.cssOutput.stylesheet = function() {
+    return Stylesheets.find({});
+  };
+
   Template.cssInput.events({
     'click #submit_css_input': function(event, template) {
       var cssInput = template.find('#css_input');
       Meteor.call('parseCSS', cssInput.value);
-      cssInput.val('');
+      cssInput.value == '';
     }
   });
 }
@@ -14,7 +18,7 @@ if (Meteor.isServer) {
   Meteor.methods({
     'parseCSS': function(cssInput) {
       var parsedCSS = parseCSS(cssInput);
-      Stylesheets.insert(parsedCSS);
+      Stylesheets.insert(parsedCSS.stylesheet);
     }
   });
 }
